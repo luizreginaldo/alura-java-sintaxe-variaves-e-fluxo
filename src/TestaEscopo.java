88b9b5ef83ddcd4490befef800aac0863be1ee83
Luiz Reginaldo Silva <contato@luizreginaldo.com>
2021-12-20 22:09:24 -0300

    escopo e inicialização de variáveis

---
+++ src/TestaEscopo.java
@@ -0,0 +1,19 @@
+
+public class TestaEscopo {
+
+	public static void main(String[] args) {
+		
+		int quantidadePessoas = 3;
+		
+		boolean acompanhado;
+		
+		if (quantidadePessoas >= 2) {
+			acompanhado = true;
+		} else {
+			acompanhado = false;
+		}
+		
+		System.out.println("valor de acompanhado = " + acompanhado);
+		
+	}
+}
